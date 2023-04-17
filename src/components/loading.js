786bafa194bc2dc219ea449d8a4e43e5018112ac
notslok <53101134+notslok@users.noreply.github.com>
2023-04-18 02:48:54 +0530

    Refactored loading to function based component

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import load from '../loading.gif'
 
-export class loading extends Component {
-  render() {
+const loading = () => {
+  
     return (
       <div className='text-center'>
         <img className="my-3" src={load} alt="loading..."/>
       </div>
     )
-  }
 }
 
 export default loading
